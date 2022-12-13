4495b83c17b0bd49dd42758b64a77a30604f1895
Fgh1277 <119784183+Fgh1277@users.noreply.github.com>
2022-12-13 10:36:33 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿Skip to content
+
+#include <iostream>
+
+using namespace std;
+
+double my_pow(double num, unsigned int deg)
+{
+	if (deg == 0)
+		return 1;
+	else if (deg == 1)
+		return num;
+	else if (deg % 2 == 0)
+		return my_pow(num * num, deg / 2);
+	else
+		return my_pow(num * num, deg / 2) * num;
+}
+
+int main()
+{
+	double num;
+	unsigned int deg;
+	cout << "Enter number: ";
+	cin >> num; cout << "Enter degree: ";
+	cin >> deg;
+	num = my_pow(num, deg);
+	cout << "Result: " << num;
+}
+
